2161017e79cb377bdb8ab77f5b76c2957802d712
myshkins <alex@ak0.io>
2023-04-07 10:41:11 -0400

    launchable

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,15 +2,17 @@ import "./ProjectCard.css";
 
 const ProjectCard = (props) => {
   return (
-    <a href={props.link}>
-      <div className="project-card">
+    <div className="project-card">
+      <a href={props.link}>
         <h2>{props.title}</h2>
-        <div className="project-card-inner">
+      </a>
+      <div className="project-card-inner">
+        <a href={props.link}>
           <img className="project-card-img" src={props.img} />
-          {props.text}
-        </div>
+        </a>
+        {props.text}
       </div>
-    </a>
+    </div>
   );
 };
 
